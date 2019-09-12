d35e90026d43721ea0247d90e182c2dfc0bf6001
JakubSto <stolarczyk.jakub@gmail.com>
2019-09-12 21:30:23 +0200

    some corrections for clean code

--- DelateVovel.java
+++ DelateVovel.java
@@ -1,12 +1,15 @@
+import java.util.List;
+
 public class DelateVovel {
     static String delateVovel(String words, int chars) {
         int counterOfChars = words.length();
         int charsToDelate = counterOfChars - chars;
         words = words.toLowerCase();
 
+        List<Character> vowels = List.of('a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'ó', 'y');
+
         for (int i = 1; i < words.length(); i++) {
-            if (words.charAt(i) == 'a' || words.charAt(i) == 'ą' || words.charAt(i) == 'e' || words.charAt(i) == 'ę'|| words.charAt(i) == 'i'
-                    || words.charAt(i) == 'o' || words.charAt(i) == 'u' || words.charAt(i) == 'ó' || words.charAt(i) == 'y') {
+            if (vowels.contains(words.charAt(i))) {
                 StringBuffer str = new StringBuffer(words);
                 words = str.delete(i, i + 1).toString();
                i = checkCharsToDelate(--charsToDelate,--i, counterOfChars);
--- Main.java
+++ Main.java
@@ -11,35 +11,33 @@ public class Main {
 
         // ----------------------------------------------------------- //
         int i = 1;
-        switch(i){
+        switch (i) {
             case 1:
-                if (finished(words,chars))
+                if (finished(words, chars))
                     break;
             case 2:
                 words = DelateChar.delateChar(words, chars);
                 System.out.println(words);
-                if (finished(words,chars))
+                if (finished(words, chars))
                     break;
             case 3:
-                words = DelateNumbers.delateNumber(words,chars);
+                words = DelateNumbers.delateNumber(words, chars);
                 System.out.println(words);
-                if (finished(words,chars))
+                if (finished(words, chars))
                     break;
             case 4:
-                words = DelateVovel.delateVovel(words,chars);
+                words = DelateVovel.delateVovel(words, chars);
                 System.out.println(words);
-                if (finished(words,chars))
+                if (finished(words, chars))
                     break;
             case 5:
-                words = DelateLetter.delateLetter(words,chars);
+                words = DelateLetter.delateLetter(words, chars);
         }
         System.out.println(words);
 
     }
-    static boolean finished (String words, int chars){
-        if (words.length() <= chars){
-            return true;
-        }
-        return false;
+
+    static boolean finished(String words, int chars) {
+        return words.length() <= chars;
     }
 }
